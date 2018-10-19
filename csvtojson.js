5ad5fe9f9aee5c63aec1b21bbe1a6ef7e95aa159
Gianpiero <gianpiero.venditti@gmail.com>
2018-10-19 19:49:43 +0200

    second commit on the master branch

--- csvtojson.js
+++ csvtojson.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const csv = require('csvtojson')
 
-// This is the master
+// This is the master: second commit
 
 const converter = () => {
 	const csvFilePath  = path.join(__dirname, process.argv[2])
